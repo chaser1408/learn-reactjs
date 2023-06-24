bf213a9b6aee6fabec8b9fdd1548cec7c0512b44
PhuSang <tpsang.1408@gmail.com>
2023-06-24 23:37:04 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Trương Phú Sang</p>
+                <p>Trương Phú Sang vừa code - 11:36 - 24/06/2023</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
